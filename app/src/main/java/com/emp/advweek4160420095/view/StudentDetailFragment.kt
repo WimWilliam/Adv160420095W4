a1b0a812fd684a0e79a188ddf1cd5a89dc4e557e
Wim <guglielmoallesandrow@gmail.com>
2024-04-04 14:35:41 +0700

    init

--- app/src/main/java/com/emp/advweek4160420095/view/StudentDetailFragment.kt
+++ app/src/main/java/com/emp/advweek4160420095/view/StudentDetailFragment.kt
@@ -5,21 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.emp.advweek4160420095.R
+import com.emp.advweek4160420095.databinding.FragmentStudentDetailBinding
+import com.example.advweek4160420095.viewmodel.DetailViewModel
+import com.google.android.material.textfield.TextInputEditText
 
 
 class StudentDetailFragment : Fragment() {
-
-
-
+    private lateinit var detailViewModel: DetailViewModel
+    private lateinit var binding: FragmentStudentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+        binding = FragmentStudentDetailBinding.inflate(inflater,container, false)
+        return binding.root
     }
 
+    fun observeViewModel() {
+        detailViewModel.studentLD.observe(viewLifecycleOwner, Observer {
+            var student = it
+            binding.btnUpdate?.setOnClickListener {
+                Observable.timer(5, TimeUnit.SECONDS)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe {
+                        Log.d("Messages", "five seconds")
+                        MainActivity.showNotification(student.name.toString(),
+                            "A new notification created",
+                            R.drawable.baseline_person_add_24)
+                    }
+            }
+        }
 
+    }
 }
--- app/src/main/java/com/emp/advweek4160420095/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/emp/advweek4160420095/viewmodel/DetailViewModel.kt
@@ -1,4 +1,25 @@
-package com.example.advweek4160420105.viewmodel
+package com.example.advweek4160420095.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.emp.advweek4160420095.model.Student
+import com.google.gson.Gson
 
 class DetailViewModel {
+    val studentLD= MutableLiveData<Student>()
+
+
+    fun fetch(student_id:String) {
+
+//        queue= Volley.newRequestQueue(getApplication())
+        val student1 = Student(
+            "16055", "Nonie", "1998/03/28", "5718444778",
+            "http://dummyimage.com/75x100.jpg/cc0000/ffffff")
+            studentLD.value = student1
+
+    }
 }
